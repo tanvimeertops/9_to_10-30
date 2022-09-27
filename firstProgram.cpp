24799cf8e787aa2c1469c901ba2dc90f173a2e03
tanvi <tanvimeer.tops@gmail.com>
2022-09-27 10:32:26 +0530

    27 sep

---
+++ firstProgram.cpp
@@ -0,0 +1,26 @@
+#include<iostream>//input output stream
+using namespace std;
+//:: scope resolution operator
+
+// unix (compiler) :okay program return 0,
+//error return 1.
+
+//wap  in c++ for sum of two nos.
+int main(){
+int a,b;
+string name;
+// std::cout<<"Enter a:";
+// cin>>a;
+// std::cout<<"enter b:";
+// cin>>b;
+
+// cout<<endl<<"sum:"<<a+b;
+
+cout<<"\nenter name:";
+getline(cin,name);
+
+
+cout<<"\nentered name:"<<name;
+
+
+}
---
+++ oopConcept.cpp
@@ -0,0 +1,40 @@
+/*oops (object oriented programming)
+1. class : is a collection data member(variable) and member function(func).
+2. object : gives permission to access the functionality of class.
+3. inheritance
+4. polymorphism
+5. encapsulation
+6. abstraction
+
+access specifier: it gives access.
+1.private
+2.public
+3.protected
+ */
+/*WAP to create simple calculator using class*/
+#include<iostream>
+using namespace std;
+class Tops{
+private:
+int a,b;
+public:
+void getdata(){
+    cout<<"enter a:";
+    cin>>a;
+    cout<<"enter b:";
+    cin>>b;
+}
+void calc(){
+   cout<<"sum:"<<a+b<<endl;
+       cout<<"sub:"<<a-b<<endl;
+          cout<<"multi:"<<a*b<<endl;
+             cout<<"div:"<<a/b<<endl;
+
+}
+};
+int main(){
+    Tops obj;//object create
+    obj.getdata();
+    obj.calc();
+}
+
